661dcd96941751b73baa76c5c16714d4121086b2
memon-gulam-45 <memongulam45@gmail.com>
2025-11-10 20:30:54 +0530

    Upload all files on GitHub

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+
+  const [task, setTask] = useState([]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const copyTask = [...task];
+
+    copyTask.push({ title, details });
+
+    setTask(copyTask);
+
+    setTitle("");
+    setDetails("");
+  };
+
+  return (
+    <div className="h-screen overflow-hidden lg:flex bg-black text-white">
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+        action=""
+        className="flex gap-5 lg:w-1/2  flex-col items-start p-10"
+      >
+        <h1 className="text-4xl font-bold">Add Notes</h1>
+
+        <input
+          type="text"
+          placeholder="Title"
+          className="px-5 w-full font-medium py-2 border-2 outline-none rounded"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+
+        <textarea
+          placeholder="Write Details Here"
+          className="px-5 w-full font-medium h-32 py-2 border-2 outline-none rounded"
+          value={details}
+          onChange={(e) => {
+            setDetails(e.target.value);
+          }}
+        ></textarea>
+
+        <button className="bg-gray-400 cursor-pointer active:bg-gray-900 active:text-white active:scale-95 hover:bg-gray-200 w-full outline-none text-black px-5 py-2">
+          Add Note
+        </button>
+      </form>
+      <div className="lg:w-1/2 lg:border-l-2 p-10 ">
+        <h1 className="text-4xl font-bold">Recent Notes</h1>
+        <div className="flex flex-wrap gap-5 mt-5 h-full overflow-auto">
+          {task.map(function (elem, idx) {
+            return (
+              <div
+                key={idx}
+                className="h-52 w-40 rounded-2xl bg-white text-black p-4"
+              >
+                <h2 className="leading-tight text-xl font-bold">
+                  {elem.title}
+                </h2>
+                <p className="mt-4 leading-tight font-medium text-gray-600">
+                  {elem.details}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
